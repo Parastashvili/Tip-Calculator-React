cbae9583f749d7e414d797d2c4804a464cc0beb8
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-05-20 19:17:43 +0400

    updated percantages

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import logo from "./images/logo.svg";
 import dollarLogo from "./images/icon-dollar.svg";
 import personLogo from "./images/icon-person.svg";
 import BillAndPeople from "./BillAndPeople";
-import Percents from "./Percents";
+import Percent from "./Percents";
 import Summer from "./Sum";
+
+const percents = [5, 10, 15, 25, 50];
 
 function App() {
   return (
@@ -17,14 +19,7 @@ function App() {
         <BillAndPeople name="Bill" image={dollarLogo} imageAlt="dollar-logo" />
         <div>
           <h5 className="selectTip">Select Tip %</h5>
-          <div className="percentContainer">
-            <Percents count="5%" value={5} />
-            <Percents count="10%" value={10} />
-            <Percents count="15%" value={15} />
-            <Percents count="25%" value={25} />
-            <Percents count="50%" value={50} />
-            <Percents count="Custom" value={10} />
-          </div>
+            <Percent percents={percents} />
         </div>
         <BillAndPeople
           name="Number of People"
--- src/Percents.js
+++ src/Percents.js
@@ -1,25 +1,56 @@
 import "./Percents.css";
-import { useState } from "react";
-const Percents = (props) => {
+import { useState, useEffect } from "react";
+
+function Percent(props) {
+  const [selectedPercent, setSelectedPercent] = useState(null);
+  const clicker = (percents) => {
+    setSelectedPercent(percents);
+  };
+  useEffect(() => {
+    if (selectedPercent !== "Custom") {
+      if (
+        selectedPercent == 5 ||
+        selectedPercent == 10 ||
+        selectedPercent == 15 ||
+        selectedPercent == 25 ||
+        selectedPercent == 50
+      ) {
+        document.querySelector(".customInput").value = "";
+      } else {
+        document.querySelector(".customInput").value = selectedPercent;
+      }
+    }
+  });
+  console.log(selectedPercent);
   return (
-    <div>
-      <button
-        className={props.count === "Custom" ? "percentText" : "percentTextNum"}
-        value={props.value}
-      >
-        {props.count === "Custom" ? (
-          <input
-            className="customInput"
-            type="number"
-            min="0"
-            max="100"
-            placeholder="Custom"
-          />
-        ) : (
-          props.count
-        )}
-      </button>
+    <div className="percentContainer">
+      {props.percents.map((percents, index) => (
+        <button
+          className={`${
+            percents !== "Custom" ? "percentTextNum" : "percentText"
+          } ${
+            selectedPercent === percents && selectedPercent !== "Custom"
+              ? "percentTextNumA"
+              : ""
+          }`}
+          key={index}
+          value={percents}
+          onClick={() => {
+            clicker(percents);
+          }}
+        >
+          {percents === "Custom" ? `${percents}` : `${percents}%`}
+        </button>
+      ))}
+      <input
+        className="customInput"
+        type="number"
+        min="0"
+        max="100"
+        placeholder="Custom"
+        onChange={(e) => setSelectedPercent(Number(e.target.value))}
+      />
     </div>
   );
-};
-export default Percents;
+}
+export default Percent;
